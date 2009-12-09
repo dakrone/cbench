90e02db37de9653e0b7fce77728d6a5497410d8e
dakrone <lee@writequit.org>
2009-12-09 14:06:09 -0700

    update project.clj with site info.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject org.clojars.thnetos/cbench "1.0.0"
-  :description "Run benchmarks of code, kinda like Ruby's benchmark function."
+  :description "Run benchmarks of code, kinda like Ruby's benchmark function. See http://github.com/dakrone/cbench for full info."
   :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
